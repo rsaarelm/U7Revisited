69396585a9968d8e1588141b37ddf50a77b0527f
Risto Saarelma <risto.saarelma@iki.fi>
2025-03-02 16:41:34 +0200

    Blender script tool for converting models to glTF

---
+++ tools/obj2gltf.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env -S blender -b -P
+
+# Blender command-line script to convert Wavefront OBJ files to glTF.
+#
+# Usage:
+#   obj2gltf.py -- whatsit.obj
+
+import argparse
+import bpy
+import os
+import sys
+
+# Get command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("infile", help="Input OBJ file")
+parser.add_argument("--json", action="store_true", help="Output separate JSON glTF")
+parser.add_argument("outfile", nargs="?", default=None, help="glTF output file path (optional)")
+
+try:
+    argv = sys.argv
+    # Get all args after '--'.
+    # The custom arguments must be preceded with '--' or Blender will
+    # interpret them as its own arguments.
+    argv = argv[argv.index("--") + 1:]
+
+    args = parser.parse_args(argv)
+
+    if args.outfile is None:
+        outfile = os.path.splitext(args.infile)[0]
+    else:
+        outfile = args.outfile
+except:
+    print("Usage: obj2gltf.py -- <input_file> <output_file>")
+    sys.exit(1)
+
+# Clear default scene
+bpy.ops.object.select_all(action='SELECT')
+bpy.ops.object.delete()
+
+# Import OBJ file
+bpy.ops.wm.obj_import(filepath=args.infile)
+
+# Bake transformation directly into the mesh.
+bpy.ops.object.transform_apply(rotation=True)
+
+kwargs = {}
+# Output binary or separate glTF based on command line flags.
+if args.json:
+    kwargs['export_format'] = 'GLTF_SEPARATE'
+
+# Export the glTF
+bpy.ops.export_scene.gltf(filepath=outfile, **kwargs)
