4e7f713923ac3186967779926f8ef10cfcda4b87
Risto Saarelma <risto.saarelma@iki.fi>
2025-03-07 09:41:45 +0200

    Animating version of model viewer

--- Examples/ModelViewer.cpp
+++ Examples/ModelViewer.cpp
@@ -1,4 +1,6 @@
+#include <cstdint>
 #include <cstdio>
+#include <cstring>
 #include <map>
 #include <vector>
 #include <array>
@@ -66,17 +68,16 @@ Mesh makeShellMesh(float weight, Mesh* source) {
         normals[key] = Vector3Normalize(normal);
     }
 
-    // I hate C++.
     mesh.vertices = (float*)RL_MALLOC(mesh.vertexCount * 3 * sizeof(float));
     mesh.texcoords = (float*)RL_MALLOC(mesh.vertexCount * 2 * sizeof(float));
-    mesh.normals = (float*)RL_MALLOC(mesh.vertexCount * 3 * sizeof(float));
-    mesh.indices = (unsigned short*)RL_MALLOC(mesh.triangleCount * 3 *
-                                              sizeof(unsigned short));
+    // Shell doesn't need any normals of it's own since it's always flat
+    // black.
+    mesh.normals = nullptr;
+    mesh.indices =
+        (uint16_t*)RL_MALLOC(mesh.triangleCount * 3 * sizeof(uint16_t));
 
     for (int i = 0; i < mesh.vertexCount * 2; i++)
         mesh.texcoords[i] = source->texcoords[i];
-    for (int i = 0; i < mesh.vertexCount * 3; i++)
-        mesh.normals[i] = source->normals[i];
 
     // Offset vertices along normals.
     for (int i = 0; i < mesh.vertexCount * 3; i += 3) {
@@ -98,21 +99,43 @@ Mesh makeShellMesh(float weight, Mesh* source) {
         mesh.indices[i + 2] = source->indices[i + 1];
     }
 
-    // TODO: Need to clone animations too when we're making shells for
-    // animated meshes...
+    // Set up animation stuff.
+    if (source->animVertices) {
+        mesh.animVertices = (float*)RL_MALLOC(mesh.vertexCount * 3 * sizeof(float));
+        memcpy(mesh.animVertices, mesh.vertices,
+            mesh.vertexCount * 3 * sizeof(float));
+    }
+
+    mesh.animNormals = nullptr;
+
+    // Copy skinning data from source mesh for animations.
+    if (source->boneIds && source->boneWeights) {
+        mesh.boneIds = (uint8_t*)RL_MALLOC(mesh.vertexCount * 4 * sizeof(uint8_t));
+        mesh.boneWeights = (float*)RL_MALLOC(mesh.vertexCount * 4 * sizeof(float));
+        memcpy(mesh.boneIds, source->boneIds,
+            mesh.vertexCount * 4 * sizeof(uint8_t));
+        memcpy(mesh.boneWeights, source->boneWeights,
+            mesh.vertexCount * 4 * sizeof(float));
+    }
 
     UploadMesh(&mesh, false);
     return mesh;
 }
 
-Model makeShell(float weight, Mesh* source) {
-    Mesh mesh = makeShellMesh(weight, source);
-    Model model = LoadModelFromMesh(mesh);
-    // Turn it black.
-    for (auto i = 0; i < model.materialCount; i++) {
-        model.materials[i].maps[MATERIAL_MAP_DIFFUSE].color = BLACK;
+/// Turn a model in-place into an outline shell.
+void shellifyModel(float weight, Model* model) {
+    model->transform = MatrixIdentity();
+    for (auto i = 0; i < model->meshCount; i++) {
+        Mesh oldMesh = model->meshes[i];
+        Mesh shell = makeShellMesh(weight, &model->meshes[i]);
+        model->meshes[i] = shell;
+        UnloadMesh(oldMesh);
     }
-    return model;
+
+    // Turn it black.
+    for (auto i = 0; i < model->materialCount; i++) {
+        model->materials[i].maps[MATERIAL_MAP_DIFFUSE].color = BLACK;
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -129,6 +152,10 @@ int main(int argc, char* argv[]) {
     // Load glTF model from file.
     Model model = LoadModel(argv[1]);
 
+    int animsCount = 0;
+    ModelAnimation* anims = LoadModelAnimations(argv[1], &animsCount);
+    printf("Loaded %d animations.\n", animsCount);
+
     // Set up shaders for lighting.
     Shader shader = LoadShader("Examples/model.vs", "Examples/model.fs");
 
@@ -138,7 +165,7 @@ int main(int argc, char* argv[]) {
 
     // Set up directional light
     int lightDirLoc = GetShaderLocation(shader, "lightDir");
-    Vector3 lightDir = {2.0f, -4.0f, 2.0f};
+    Vector3 lightDir = {2.0f, -4.0f, -2.0f};
     SetShaderValue(shader, lightDirLoc, &lightDir, SHADER_UNIFORM_VEC3);
 
     Vector3 ambientLight = {0.2f, 0.2f, 0.2f};
@@ -149,9 +176,8 @@ int main(int argc, char* argv[]) {
 
     // Make the inverted shell model for drawing a black outline.
 
-    // XXX: This only does the first mesh, so it won't work on multi-mesh
-    // models.
-    Model edgeShell = makeShell(0.05, &model.meshes[0]);
+    Model edgeShell = LoadModel(argv[1]);
+    shellifyModel(0.05, &edgeShell);
 
     // Camera setup.
     Camera camera = {{0}};
@@ -164,16 +190,27 @@ int main(int argc, char* argv[]) {
     Camera tileCamera = {{0}};
     tileCamera.position = (Vector3){0.0f, 20.0f, 0.0f};
     tileCamera.target = (Vector3){0.0f, 0.0f, 0.0f};
-    tileCamera.up = (Vector3){0.0f, 0.0f, 1.0f};
+    tileCamera.up = (Vector3){0.0f, 0.0f, -1.0f};
     tileCamera.fovy = 4.0f;
     tileCamera.projection = CAMERA_ORTHOGRAPHIC;
 
     RenderTexture2D obliqueView = LoadRenderTexture(400, 400);
 
+    int currentAnim = 0;
+    int currentFrame = 0;
+
     while (!WindowShouldClose()) {
         UpdateCamera(&camera, CAMERA_ORBITAL);
         SetShaderValue(shader, shader.locs[SHADER_LOC_VECTOR_VIEW],
                        &camera.position.x, SHADER_UNIFORM_VEC3);
+
+        if (currentAnim < animsCount) {
+            UpdateModelAnimation(model, anims[currentAnim], currentFrame);
+            UpdateModelAnimation(edgeShell, anims[currentAnim], currentFrame);
+            currentFrame++;
+            if (currentFrame >= anims[0].frameCount)
+                currentFrame = 0;
+        }
 
         // Render an inset oblique view of the model.
         BeginTextureMode(obliqueView);
@@ -183,8 +220,8 @@ int main(int argc, char* argv[]) {
             {
                 rlPushMatrix();
                 Matrix oblique = MatrixIdentity();
-                oblique.m4 = 0.5f;
-                oblique.m6 = 0.5f;
+                oblique.m4 = -0.5f;
+                oblique.m6 = -0.5f;
                 rlMultMatrixf(MatrixToFloat(oblique));
 
                 DrawModel(model, {0.0f, 0.0f, 0.0f}, 1.0f, WHITE);
@@ -212,10 +249,19 @@ int main(int argc, char* argv[]) {
                 obliqueView.texture,
                 (Rectangle){0, 0, static_cast<float>(obliqueView.texture.width),
                             static_cast<float>(-obliqueView.texture.height)},
-                (Vector2){8, 8}, WHITE);
+                (Vector2){8, 32}, WHITE);
+            // Print animation count on screen
+            if (animsCount > 0)
+                DrawText(TextFormat("Animation: %d", currentAnim), 10, 8, 20, BLACK);
         }
         EndDrawing();
+
+        // If key is pressed, cycle animation
+        if (IsKeyPressed(KEY_SPACE)) {
+            currentAnim = (currentAnim + 1) % animsCount;
+        }
     }
+
 
     UnloadShader(shader);
     CloseWindow();
